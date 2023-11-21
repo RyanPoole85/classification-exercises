9ec8a3aeb4f1ad02635a54cd268767a5141436d1
Ryan Poole <ryan.anthony.poole@gmail.com>
2023-11-21 10:41:07 -0600

    Updated

--- acquire.py
+++ acquire.py
@@ -6,8 +6,8 @@ from pydataset import data
 
 def get_titanic_data():
    
-    filename = 'titanic.csv'
-    
+    filename = 'titanic.csv_'
+    url = env.get_db_url('titanic_db')
     if os.path.exists(filename):
         print('this file exists, reading from csv')
         
@@ -23,17 +23,18 @@ def get_titanic_data():
 
 
 def get_iris_data():
-    filename = 'iris'
-    from pydataset import data
+    filename = 'iris_'
+    url = env.get_db_url('iris_db')
     
     if os.path.exists(filename):
         print('this file exists, reading from csv')
 
         df_iris = pd.read_csv(filename, index_col=0)
     else:
-        print('this file doesnt exist, creating file and saving as csv')
+        print('this file doesnt exist, reading from sql and saving as csv')
         
-        df_iris = data('iris')
+        df_iris = pd.read_sql('''select * from species
+join measurements using (species_id)''', url)
         df_iris.to_csv(filename)
         
     return df_iris
@@ -41,8 +42,8 @@ def get_iris_data():
 
 def get_telco_data():
    
-    filename = 'telco.csv'
-    
+    filename = 'telco.csv_'
+    url = env.get_db_url('telco_churn')
     if os.path.exists(filename):
         print('this file exists, reading from csv')
         
